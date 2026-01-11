8638c176225b2d5316a974949a59c1906ac09515
Klein3568 <3568335079@qq.com>
2026-01-11 11:49:37 +0800

    Add CameraMonitor route to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import Dashboard from '../views/Dashboard.vue'
 import DeviceControl from '../views/DeviceControl.vue'
 import CommandControl from '../views/CommandControl.vue'
 import Messages from '../views/Messages.vue'
+import CameraMonitor from '../views/CameraMonitor.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/message',
       name: 'message',
       component: Messages
+    },
+    {
+      path: '/camera',
+      name: 'camera',
+      component: CameraMonitor
     }
   ]
 })
